9fbd8f685d27a70d4884a711409f97fbb7f6c5f0
Mercy Housh <mercyhoush@Mercys-MacBook-Pro.local>
2019-06-25 23:06:10 -0500

    Commit with final color table view implemented along with the bonus

---
+++ Colors/Color.swift
@@ -0,0 +1,14 @@
+//
+//  Color.swift
+//  Colors
+//
+//  Created by Mercy Housh on 6/25/19.
+//  Copyright © 2019 Mercy Housh. All rights reserved.
+//
+
+import UIKit
+
+struct Color {
+    let name: String
+    let uiColor: UIColor
+}
---
+++ Colors/ColorsViewController.swift
@@ -0,0 +1,57 @@
+//
+//  ColorsViewController.swift
+//  Colors
+//
+//  Created by Mercy Housh on 6/25/19.
+//  Copyright © 2019 Mercy Housh. All rights reserved.
+//
+
+import UIKit
+
+class ColorsViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+
+    //var colors = ["red", "orange", "yellow", "green", "blue", "purple", "brown"]
+    
+    var colors = [Color(name: "red", uiColor: UIColor.red),
+                  Color(name: "orange", uiColor: UIColor.orange),
+                  Color(name: "yellow", uiColor: UIColor.yellow),
+                  Color(name: "green", uiColor: UIColor.green),
+                  Color(name: "blue", uiColor: UIColor.blue),
+                  Color(name: "purple", uiColor: UIColor.purple),
+                  Color(name: "brown", uiColor: UIColor.brown),
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return colors.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "colorCell", for: indexPath)
+        
+        cell.textLabel?.text = colors[indexPath.row].name
+        cell.backgroundColor = colors[indexPath.row].uiColor
+        
+        return cell
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
